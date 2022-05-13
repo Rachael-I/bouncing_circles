1bff89ab317e9ca15e425c7de7fe68e795213159
Rachael-I <Rachael_I@mail.com>
2022-05-14 00:08:14 +1200

    added changing fill colour to circles

--- sketch.js
+++ sketch.js
@@ -1,32 +1,56 @@
 var circle_1;
 var circle_2;
 var circle_3;
-var prettyColour;
+var pretty;
+var groovy;
 
 function setup() {
   createCanvas(400, 400);
   circle_1 = new Circle();
   circle_2 = new Circle();
   circle_3 = new Circle();
-  prettyColour = 1;
+  
+  pretty = {
+    prettyColour: 50,
+    change() {
+      if (this.prettyColour < 255) {
+        this.prettyColour = this.prettyColour + 1;
+      } else {
+        this.prettyColour = this.prettyColour - 255;
+      } 
+      return this.prettyColour;
+    }
+   };
+  
+  groovy = {
+    groovyColour: 50,
+    change() {
+      if (this.groovyColour < 105) {
+        this.groovyColour = this.groovyColour + 1;
+      } else {
+        this.groovyColour = this.groovyColour - 255;
+      } 
+      return this.groovyColour;
+    }
+   };
 }
 
 function draw() {
-  if (prettyColour < 255) {
-    prettyColour = prettyColour + 1;
-  } else {
-    prettyColour = prettyColour - 255;
-  }
+  var c = pretty.change();
+  background(c, 200, 200); 
+  noStroke();
   
-  background(prettyColour, 200, 200); 
-  
+  var d = groovy.change();
+  fill(d, d, 200);
   circle_1.draw();
   circle_1.slide();
   
+  fill(d, 200, d);
   circle_2.y = 250;
   circle_2.draw();
   circle_2.slide();
   
+  fill(200, d, d);
   circle_3.x = 350;
   circle_3.draw();
   circle_3.bounce();
@@ -37,6 +61,7 @@ function Circle() {
   this.y = 98;
   this.size = 50;
   this.changeBy = 1;
+  this.changeUp = 2;
   this.draw = function() {
     ellipse (this.x, this.y, this.size, this.size);
   };
@@ -46,15 +71,15 @@ function Circle() {
     }
   }
   this.slide = function() {
-    if (this.x > 200 || this.x < 25) {
+    if (this.x > 300 || this.x < 25) {
       this.changeBy = 0 - this.changeBy;
     }
     this.x = this.x + this.changeBy;
   }
     this.bounce = function() {
-    if (this.y > 200 || this.y < 25) {
-      this.changeBy = 0 - this.changeBy;
+    if (this.y > 375 || this.y < 25) {
+      this.changeUp = 0 - this.changeUp;
     }
-    this.y = this.y + this.changeBy;
+    this.y = this.y + this.changeUp;
   }
 }
